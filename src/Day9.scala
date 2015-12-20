29cb839a72fbb54101b542b1b01d9a164fc7d858
Montana Ruth <montanalruth@gmail.com>
2015-12-20 01:16:31 -0600

    Started working on Day 9

---
+++ src/Day9.scala
@@ -0,0 +1,19 @@
+
+/**
+  * Created by Montana Ruth on 12/20/2015.
+  */
+
+object Day9 extends App {
+  def part_1(): String = {
+
+    ???
+  }
+
+  def part_2(): String = {
+
+    ???
+  }
+
+  println(s"Part 1: ${part_1}")
+  println(s"Part 2: ${part_2}")
+}
